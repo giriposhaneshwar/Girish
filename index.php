7f1f5c454abcae7e7898b6761d4189e4eba631fb
giriposhaneshwar <egp.designs@gmail.com>
2014-12-01 23:05:20 +0530

    Basic Startup

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DC</title>
+	<link rel="stylesheet" type="text/css" href="./css/basicStyles.css">
+	<script type="text/javascript" src="./js/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="./js/jquery-ui.js"></script>
+</head>
+<body>
+
+<div class="mainContainer">
+	<div class="warper">
+		<div class="leftC">Left Container</div>
+		<div class="rightC">Right Container</div>
+	</div>
+</div>
+</body>
+</html>
